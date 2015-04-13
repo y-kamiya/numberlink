99a21b4d24b52620efa9c6a2bf3ea05173ba6ce5
Yuji Kamiya <y.kamiya0@gmail.com>
2015-04-14 08:16:39 +0900

    calc route of designated num

--- hs/numberlink-solver/src/Main.hs
+++ hs/numberlink-solver/src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.List (maximumBy)
+import Data.Function (on)
 import qualified Data.Map as M
 
 problem :: [String]
@@ -23,8 +25,22 @@ data CellType = Start | Goal | Normal deriving (Eq,Show)
 
 type Field = M.Map Pos Cell
 
+showField :: Field -> String
+showField field = chunk $ concatMap (show . getNumber . cState) $ M.elems field
+  where (_,m) = fieldSize field
+        chunk [] = []
+        chunk ss = let (a, b) = splitAt m ss
+                   in  a ++ "\n" ++ chunk b
+ 
+getNumber :: CellState -> Int
+getNumber Empty = 0
+getNumber (Number n) = n
+
 mkEmptyCell :: Cell
 mkEmptyCell = Cell Empty Normal
+
+mkCell :: Int -> Cell
+mkCell n = Cell (Number n) Normal
 
 mkStartCell :: Int -> Cell
 mkStartCell n = Cell (Number n) Start
@@ -40,8 +56,8 @@ mkField input = convert (zip [0..] $ concat input) M.empty
     convert ((id, c):ts) field 
       | c == '.'  = convert ts $ M.insert pos mkEmptyCell field
       | otherwise = convert ts $ if hasValue value 
-                                   then M.insert pos (mkStartCell value) field
-                                   else M.insert pos (mkGoalCell  value) field
+                                   then M.insert pos (mkGoalCell value) field
+                                   else M.insert pos (mkStartCell value) field
       where
         pos = (id `div` m, id `mod` n)
         value = read [c] :: Int
@@ -52,4 +68,71 @@ mkField input = convert (zip [0..] $ concat input) M.empty
 
 main :: IO ()
 main = do
-    print $ mkField problem
+    let field = mkField problem
+        answer = head $ solve $ findStart 1 field
+    print answer
+
+solve :: (Pos, Field) -> [(Pos, Field)]
+solve (pos, field) 
+  | isCompleted field = return ((-1,-1), field)
+  | otherwise = [(p,fld) | (p, fld) <- next pos field] >>= solve
+
+isCompleted :: Field -> Bool
+isCompleted field = all (not . isEmpty field) $ M.keys field
+
+next :: Pos -> Field -> [(Pos, Field)]
+next p field = case M.lookup p field of
+                 Just (Cell (Number n) Goal) -> nextStart n field
+                 _ -> map update (validNextPos p field)
+  where
+    value = getValue p field
+    update pos = (pos, M.update (\_ -> Just $ mkCell value) pos field)
+
+nextStart :: Int -> Field -> [(Pos, Field)]
+nextStart n field = if n == maxNum
+                      then [((-1,-1),field)]
+                      else [findStart (n+1) field]
+  where
+    maxNum = maximum $ map (getNumber . cState) $ M.elems field
+
+
+findStart :: Int -> Field -> (Pos, Field)
+findStart n field = let pos = head $ M.keys $ M.filter (isStartN n) field
+                    in  (pos, field)
+  where
+    isStartN n (Cell cellState cellType) = cellState == Number n && cellType == Start
+
+getValue :: Pos -> Field -> Int
+getValue p field = case M.lookup p field of
+                     Nothing -> 0
+                     Just (Cell Empty _) -> 0
+                     Just (Cell (Number n) _) -> n
+
+fieldSize :: Field -> Pos
+fieldSize field = let (n,m) = last $ M.keys field
+                  in (n+1,m+1)
+
+inField :: Field -> Pos -> Bool
+inField field (x,y) = 0 <= x && x < n && 0 <= y && y < m
+  where (n,m) = fieldSize field
+
+isEmpty :: Field -> Pos -> Bool
+isEmpty field p = case M.lookup p field of
+                    Just (Cell Empty _) -> True
+                    _ -> False
+
+isStart :: Field -> Pos -> Bool
+isStart field p = case M.lookup p field of
+                    Just (Cell _ Start) -> True
+                    _ -> False
+
+isGoalN :: Int -> Field -> Pos -> Bool
+isGoalN n field p = case M.lookup p field of
+                    Just (Cell (Number m) Goal) -> n == m
+                    _ -> False
+
+nextPos :: Pos -> [Pos]
+nextPos (x,y) = [(x+1,y),(x,y-1),(x-1,y),(x,y+1)]
+
+validNextPos :: Pos -> Field -> [Pos]
+validNextPos p field = filter (\pos -> isEmpty field pos || not (isStart field pos)) $ filter (inField field) $ nextPos p
