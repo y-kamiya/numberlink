de1d2b1e076c8754863c18b9277ec28c3bafdcf2
Yuji Kamiya <y.kamiya0@gmail.com>
2015-06-22 08:26:58 +0900

    convert to string

--- web/editor/js/editor.js
+++ web/editor/js/editor.js
@@ -5,19 +5,46 @@ var createTable = function() {
         rowNum = $('#rowNum').val(),
         colNum = $('#colNum').val();
 
-    console.log(rowNum);
-    console.log(colNum);
     for (var row = 0; row < rowNum; row++) {
         var tr = $('<tr>')
         for (var col = 0; col < colNum; col++) {
             var cellId = row * colNum + col;
-            tr.append("<td id=\"c" + cellId + "\">a</td>");
+            tr.append("<td id=\"c" + cellId + "\"><input type=\"number\"></td>");
         }
         table.append(tr);
     }
     $('#field').append(table);
 }
 
-$('#createTableButton').click(createTable);
+var changeButtonToSave = function () {
+    $(this).text('Save');
+    $(this).attr('id', 'saveButton');
+    $(this).off('click');
+    $(this).on('click', save);
+}
+
+var save = function () {
+    var rowNum = $('#rowNum').val(),
+        colNum = $('#colNum').val();
+
+    var text = "";
+    for (var row = 0; row < rowNum; row++) {
+        for (var col = 0; col < colNum; col++) {
+            var cellId = row * colNum + col,
+                value = $('#c' + cellId + " > input").val();
+            if (value == "") {
+                text += ".";
+            } else {
+                console.log(value);
+                text += String.fromCharCode(Number(value) + 96);
+            }
+        }
+    }
+    console.log(text);
+}
+
+$('#createTableButton').on('click', createTable);
+$('#createTableButton').on('click', changeButtonToSave);
+
 
 });
