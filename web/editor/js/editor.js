cdc14b1b857e16ffb69a51c9e105337e3c7c14e6
Yuji Kamiya <y.kamiya0@gmail.com>
2015-06-25 08:12:20 +0900

    separate editor from input page

--- web/editor/js/editor.js
+++ web/editor/js/editor.js
@@ -2,54 +2,9 @@ $(function () {
 
     var CHAR_CODE_OFFSET = 96;
 
-    var createTable = function() {
-        var table = $('<table>'),
-            rowNum = $('#rowNum').val(),
-            colNum = $('#colNum').val();
-
-        for (var row = 0; row < rowNum; row++) {
-            var tr = $('<tr>')
-            for (var col = 0; col < colNum; col++) {
-                var cellId = row * colNum + col;
-                tr.append("<td id=\"c" + cellId + "\"><input type=\"number\"></td>");
-            }
-            table.append(tr);
-        }
-        $('#field').append(table);
-    }
-
-    var changeButtonToSave = function () {
-        $(this).text('Save');
-        $(this).attr('id', 'saveButton');
-        $(this).off('click');
-        $(this).on('click', save);
-    }
-
-    var save = function () {
-        var rowNum = $('#rowNum').val(),
-            colNum = $('#colNum').val();
-
-        var text = "";
-        for (var row = 0; row < rowNum; row++) {
-            for (var col = 0; col < colNum; col++) {
-                var cellId = row * colNum + col,
-                    value = $('#c' + cellId + " > input").val();
-                if (value == "") {
-                    text += ".";
-                } else {
-                    text += String.fromCharCode(Number(value) + CHAR_CODE_OFFSET);
-                }
-            }
-            text += "<br>"
-        }
-        $('#output').append(text);
-        $('#load').removeClass('disabled');
-        $('#load').on('click', load);
-    }
-
     var load = function() {
         var table = $('<table>'),
-            data = $('#output').text(),
+            data = $('#input').text(),
             rowNum = $('#rowNum').val(),
             colNum = $('#colNum').val();
 
@@ -66,11 +21,12 @@ $(function () {
             }
             table.append(tr);
         }
-        $('#field > table').remove()
         $('#field').append(table);
     }
 
-    $('#createTableButton').on('click', createTable);
-    $('#createTableButton').on('click', changeButtonToSave);
+    var start = function() {
+    }
+
+    load();
 
 });
